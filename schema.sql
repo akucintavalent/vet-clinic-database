aea2eeb10401ab63e48e666afc667711a4c1b687
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-02-15 23:17:51 +0100

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+    id int primary key not null,
+    name            varchar(100) not null,
+    date_of_birth   date,
+    escape_attempts int,
+    neutered        boolean,
+    weight_kg       decimal
 );
