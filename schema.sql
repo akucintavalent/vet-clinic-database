56282f3150bf3acc8ca17e4c4d25eb1f13aae479
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-02-16 14:32:14 +0100

    Fix table schema & Add a forgotten sql query

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-    id int primary key not null,
-    name            varchar(100) not null,
-    date_of_birth   date,
-    escape_attempts int,
-    neutered        boolean,
-    weight_kg       decimal
+    id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name            VARCHAR(100) not null,
+    date_of_birth   DATE,
+    escape_attempts INT,
+    neutered        BOOLEAN,
+    weight_kg       DECIMAL
 );
