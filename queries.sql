d7003248429e7864a9c6940436dde10b79d2a01d
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-02-17 08:46:33 +0100

    Add minor changes and use alter table add column instead of changing table declaration

--- queries.sql
+++ queries.sql
@@ -11,22 +11,24 @@ SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 BEGIN;
 UPDATE animals SET species = 'unspecified';
-SELECT * FROM animals;
+SELECT species FROM animals;
 ROLLBACK;
-SELECT * FROM animals;
+SELECT species FROM animals;
 END;
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT species FROM animals;
 COMMIT;
-SELECT * FROM animals;
+SELECT species FROM animals;
 END;
 
 BEGIN;
 DELETE from animals;
+SELECT species FROM animals;
 ROLLBACK;
-SELECT * FROM animals;
+SELECT species FROM animals;
 END;
 
 BEGIN;
@@ -43,4 +45,4 @@ SELECT COUNT(*) FROM animals GROUP BY escape_attempts HAVING escape_attempts = 0
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg) AS min_weight_kg, MAX(weight_kg) AS max_weight_kg  FROM animals GROUP BY species;
-SELECT species, AVG(escape_attempts) AS avg_escape_attempts  FROM animals WHERE date_of_birth >= '1990-01-01' AND date_of_birth < '2000-01-01' GROUP BY species;
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts  FROM animals WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31' GROUP BY species;
